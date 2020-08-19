b843efd3df11efa89f0db556597da79dea600fa4
Ryan Yang <ryanqyang@gmail.com>
2020-08-18 21:56:58 -0700

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="site">
+			Compounding Pharmacy Associates and Consultants is owned and operated by Geneva Chen, Pharm.D, Ph.D. Dr. Chen has had extensive training in pharmacy compounding and consulting, and has also worked as a research scientist and pharmacy college professor. She is highly dedicated to her patients and practice, working closely with physicians and patients to achieve excellent results for each custom-made prescription.
+		</div>
+	);
+}
+
+export default App;
