fdb72849c9d7ebe1ff8e4d2c143e4dcfa069cbae
alexanderqchen <alexander.qili.chen@gmail.com>
2021-01-27 22:10:44 -0500

    [BUG FIX] Hardcode logo size

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,34 +1,51 @@
-import React, {useState, Fragment} from 'react';
-import {Link, withRouter} from 'react-router-dom';
-import {HashLink} from 'react-router-hash-link';
-import Logo from '../assets/logo.png';
-import {ReactComponent as HamburgerIcon} from './hamburger.svg';
-import './Navigation.scss';
+import React, { useState, Fragment } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import Logo from "../assets/logo.png";
+import { ReactComponent as HamburgerIcon } from "./hamburger.svg";
+import "./Navigation.scss";
 
-const Navigation = ({history}) => {
-    const [open, setOpen] = useState(false);
-    const toggleMenu = () => {
-        setOpen(!open);
-    }
-    const redirectHome = () => {
-        history.push('/');
-    }
-    return (
-        <Fragment>
-            <div className={`navigation--wrapper ${open ? 'open' : ''}`}>
-                <img src={Logo} alt="logo" onClick={redirectHome}/>
-                <div className="spacer"></div>
-                <HashLink onClick={toggleMenu} smooth to='/#services'>services</HashLink>
-                <HashLink onClick={toggleMenu} smooth to='/#testimonials'>testimonials</HashLink>
-                <HashLink onClick={toggleMenu} smooth to='/#about-us'>about</HashLink>
-                <HashLink onClick={toggleMenu} smooth to='/#contact-us'>contact</HashLink>
-                <button onClick={toggleMenu}>
-                    <Link className="btn" to={process.env.PUBLIC_URL + '/refill'}>refill</Link>
-                </button>
-            </div>
-            <div className="navigation-mobile--icon" onClick={toggleMenu}><HamburgerIcon /></div>
-        </Fragment>
-    )
-}
+const Navigation = ({ history }) => {
+  const [open, setOpen] = useState(false);
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+  const redirectHome = () => {
+    history.push("/");
+  };
+  return (
+    <Fragment>
+      <div className={`navigation--wrapper ${open ? "open" : ""}`}>
+        <img
+          src={Logo}
+          alt="logo"
+          onClick={redirectHome}
+          className="navigation--logo"
+        />
+        <div className="spacer"></div>
+        <HashLink onClick={toggleMenu} smooth to="/#services">
+          services
+        </HashLink>
+        <HashLink onClick={toggleMenu} smooth to="/#testimonials">
+          testimonials
+        </HashLink>
+        <HashLink onClick={toggleMenu} smooth to="/#about-us">
+          about
+        </HashLink>
+        <HashLink onClick={toggleMenu} smooth to="/#contact-us">
+          contact
+        </HashLink>
+        <button onClick={toggleMenu}>
+          <Link className="btn" to={process.env.PUBLIC_URL + "/refill"}>
+            refill
+          </Link>
+        </button>
+      </div>
+      <div className="navigation-mobile--icon" onClick={toggleMenu}>
+        <HamburgerIcon />
+      </div>
+    </Fragment>
+  );
+};
 
 export default withRouter(Navigation);
